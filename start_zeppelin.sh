96b01094ccd3c0b6c3f38fd6f745411d0a90e829
Viacheslav Kalashnikov <xemuliam@users.noreply.github.com>
2017-02-20 15:13:35 +0200

    0.7.0

--- start_zeppelin.sh
+++ start_zeppelin.sh
@@ -2,4 +2,18 @@
 
 set -e
 
+# Zeppelin config
+if [ ! -z "$HOMESCREEN" ]; then
+  cp ${ZEPPELIN_HOME}/conf/zeppelin-env.sh.template ${ZEPPELIN_HOME}/conf/zeppelin-env.sh
+  echo "export ZEPPELIN_NOTEBOOK_HOMESCREEN=${HOMESCREEN}" >> ${ZEPPELIN_HOME}/conf/zeppelin-env.sh
+  
+  if [ ! -z "$HOMESCREEN_HIDE" ]; then
+    echo "export ZEPPELIN_NOTEBOOK_HOMESCREEN_HIDE=true" >> ${ZEPPELIN_HOME}/conf/zeppelin-env.sh
+  else
+    echo "export ZEPPELIN_NOTEBOOK_HOMESCREEN_HIDE=false" >> ${ZEPPELIN_HOME}/conf/zeppelin-env.sh
+  fi
+else
+  rm -f ${ZEPPELIN_HOME}/conf/zeppelin-env.sh
+fi
+
 ${ZEPPELIN_HOME}/bin/zeppelin-daemon.sh start; sleep 5; tail -F ${ZEPPELIN_HOME}/logs/zeppelin-*.log
